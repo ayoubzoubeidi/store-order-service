beed1b62a9afb77fcd204b913a6cee7074ec8927
ayoub <ayoubzoubeidi@gmail.com>
2021-08-15 15:18:41 +0100

    Initial project commit

---
+++ src/test/java/com/maz/order/StoreOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maz.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreOrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
