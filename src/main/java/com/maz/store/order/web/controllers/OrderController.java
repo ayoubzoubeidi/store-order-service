56b90890ff1d8dc0bf46c772d8a0851512ba993b
Ayoub <ayoubzoubeidi@gmail.com>
2021-10-15 11:02:36 +0100

    misc changes and import optimizations

--- src/main/java/com/maz/store/order/web/controllers/OrderController.java
+++ src/main/java/com/maz/store/order/web/controllers/OrderController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
 @Controller
-@RequestMapping("/orders")
+@RequestMapping("/api/v1/orders")
 @RequiredArgsConstructor
 public class OrderController {
 
